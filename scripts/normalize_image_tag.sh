8871ee69faf4dc5e0e9d4916032fecf0070976b1
hoatle <hoatle@teracy.com>
2020-03-06 14:35:08 +0700

    test

--- scripts/normalize_image_tag.sh
+++ scripts/normalize_image_tag.sh
@@ -7,9 +7,9 @@ BRANCH_NAME=$1
 # TODO(hoatle): remove job type
 # job_type convention
 if [ -z "${JOB_TYPE}" ]; then
-  export IMG_TAG=$(echo "$BRANCH_NAME" | awk '{print tolower($0)}' | sed -e 's/[\/]/-/g' | sed -e 's/[\#]//g');
+  export IMG_TAG=$(echo "$BRANCH_NAME" | awk '{print tolower($0)}' | sed -e 's/[\/]/-/g' | sed -e 's/[\#]//g' | sed -e 's/[\.]/_/g');
 else
-  export IMG_TAG=$(echo "$JOB_TYPE" | awk '{print tolower($0)}' | sed -e 's/[\/]/-/g' | sed -e 's/[\#]//g');
+  export IMG_TAG=$(echo "$JOB_TYPE" | awk '{print tolower($0)}' | sed -e 's/[\/]/-/g' | sed -e 's/[\#]//g' | sed -e 's/[\.]/_/g');
 fi
 
 echo "\$IMG_TAG: $IMG_TAG";
